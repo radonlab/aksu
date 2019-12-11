bdb5ab8cdad4396836996fafd6626d9c00c5ed5c
skylerlee <skyler.ac.lee@gmail.com>
2019-12-11 15:24:13 +0000

    feat: update main

--- cmd/aksu.c
+++ cmd/aksu.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+#include "event2/event.h"
+#include "event2/http.h"
 
 int main(int argc, const char** argv) {
-  printf("It works!");
+  struct event_base* base = event_base_new();
+  struct evhttp* serv = evhttp_new(base);
+  evhttp_free(serv);
+  event_base_free(base);
   return 0;
 }
